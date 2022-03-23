d2b99144a8797e7382970d400b88b5489269d050
zce <w@zce.me>
2022-03-23 15:12:31 +0800

    feat: caz v1.0.0-alpha

--- cli.js
+++ cli.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
-
 const { default: caz } = require('caz')
-
 // template root
 const template = __dirname
-
 // project directory
 const project = process.argv[2]
-
 // scaffolding by caz...
 caz(template, project).catch(console.error)
